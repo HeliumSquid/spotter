c705fd942e08a1d14b00b5f1f32d92e8710d85b0
HeliumSquid <HeliumSquid@users.noreply.github.com>
2016-02-28 17:01:29 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Spotter
+> A Chrome extension that replaces +1 comments on GitHub.
+
+![Spotter](icon.png)
+
+We have all been [dreaming](https://i.imgur.com/k9RAFbW.gif) of this.
+
+Well, here it is! \ (•◡•) / Spotter, the all new Chrome extension that replaces all +1 comments on GitHub with ... well ... other comments.
+
+## Install
+
+Download the [latest release](#).
---
+++ walk.js
@@ -0,0 +1,20 @@
+function walk(node) {
+    node = node || document.body;
+    if (node.nodeType == 3) {
+        // List of words to replace +1 with.
+        var words = ["Butts", "HISSSSSSSSSSS", "What's my name again?", "HELP! Where am I?", "You can do that with Jake Weary!", "Git lost!",
+        "This is a secret message because the NSA is watching us.", "Why not Zoidberg? (V)_(@;;;;@)_(V)"];
+        // Select a random word.
+        var rand = words[Math.floor(Math.random() * words.length)];
+        node.nodeValue = node.nodeValue.split('+1').join(rand);
+    } else {
+        var nodes = node.childNodes;
+        if (nodes) {
+            var i = nodes.length;
+            while (i--) walk(nodes[i]);
+        }
+    }
+}
+
+walk();
+// Or just take the escalator.
